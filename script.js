97cda5db81908fc67bf41b86c4accd2442a48fe7
CHKL98 <christopherschel@hotmail.com>
2022-11-08 10:25:17 +0100

    Tilføjede en knap

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ element.addEventListener('click', myFunction);
 function myFunction() {
 	alert('Hello World!');
 }
+
+let element1 = document.getElementById('myBtn1');
+element.addEventListener('Prøv og tryk', myFunction);
+function myFunction() {
+	alert('Nøjj');
+}
+
