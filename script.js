0c0b9dc17b2f23b6fbdf9d223018b3622b08584c
Måge <alona.chaika@icloud.com>
2022-11-14 12:31:05 +0100

    Input

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function showModal() {
 	let title = form.getElementsByClassName('form-title')[0];
 	// title.innerHTML = 'hello';
 }
+
+function getInputValue(event) {  // A method is used to get input from titel
+
+	event.preventDefault()
+	let value = document.getElementsByClassName("form-title")[0]; //get input from titel box
+	console.log(value.value);     // Display the value
+	let valuearea = document.getElementsByClassName("task")[0]; //get input from task box
+	console.log(valuearea.value);     // Display the value
+  }
+
