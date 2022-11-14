b6f59bd20bcb797eee776af971e1eb7ba6720c50
Hong Duc Cai <duchongcai@gmail.com>
2022-11-14 11:08:13 +0100

    showModal

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-let element = document.getElementById('myBtn');
-element.addEventListener('click', myFunction);
-function myFunction() {
-	alert('Hello World!');
+// let element = document.getElementById('myBtn');
+// element.addEventListener('click', myFunction);
+// function myFunction() {
+// 	alert('Hello World!');
+// }
+
+// let button = document.getElementsByClassName('add-task');
+// button.addEventListener('click', showModal);
+
+function showModal() {
+	let form = document.getElementsByClassName('form')[0];
+	form.classList.toggle('showModal');
+	// form.classList.add('showModal');
+	let title = form.getElementsByClassName('form-title')[0];
+	// title.innerHTML = 'hello';
 }
-
-
-
