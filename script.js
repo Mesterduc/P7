a6b502506de12c5387bcbd967d511804f823c4df
Dzenan <dzenanbajic@hotmail.com>
2022-11-16 09:51:57 +0100

    category box

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function showTaskBox() {
 	let form = document.getElementsByClassName('form')[0];
 	form.classList.toggle('showTaskBox');
-	// form.classList.add('showTaskBox');
 	form.reset()
 	
 }
@@ -17,12 +16,12 @@ function getInputValue(event) {
 
 
 
-
+let todo = document.getElementsByClassName("toDo")[0]
 
 	//Implement the taskbox (section) from HTLM and define it as taskbox in script
 	let taskbox = document.createElement("section"); //Define the taskbox created in HTML section
 	taskbox.classList.add("id", "mySection");
-    document.body.append(taskbox);
+    todo.append(taskbox);
 
 	let h4 = document.createElement("h4"); //Users input in formElement (title) will show on the screen 
 	taskbox.append(h4);
@@ -63,4 +62,25 @@ function getInputValue(event) {
 	
 	showTaskBox();
 	
+}
+
+function showTaskCategory() {
+	let form = document.getElementsByClassName('category-form')[0];
+	form.classList.toggle('showCategoryBox');
+	form.reset()
+}
+
+function getCategoryValue(event) {
+	// A method is used to get input from titel
+	event.preventDefault();
+	let formCategory = document.getElementsByClassName('form-title')[0];
+	
+	let categorybox = document.createElement("section"); //Define the taskbox created in HTML section
+	categorybox.classList.add("id", "mySection");
+    document.body.append(categorybox);
+
+	let h4 = document.createElement("nput"); //Users input in formElement (title) will show on the screen 
+	categorybox.append(h4);
+	h4.innerHTML = formElement.value;
+	showTaskCategory()
 }
