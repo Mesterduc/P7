ac2c3d9d222ef43166a03ec4cdf554d4ed7ff6c7
Hong Duc Cai <hongduccai@gmail.com>
2022-11-21 12:42:10 +0100

    comment on how to run the backend

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+# go into backend folder and run below in terminal:
+# python3 -m venv venv
+# . venv/bin/activate
+# pip install Flask
+
+# for running the server:
+# flask --app app run
+
 
 @app.route("/")
 def hello_world():
